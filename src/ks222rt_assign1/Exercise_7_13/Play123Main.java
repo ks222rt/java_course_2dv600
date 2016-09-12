1bf303f23b08ebf0a7875d009590593be8ff67db
Kristoffer Svensson <ks222rt@student.lnu.se>
2016-09-12 16:35:17 +0200

    made a change to the play123Main class and began with assignment 2

--- src/ks222rt_assign1/Exercise_7_13/Play123Main.java
+++ src/ks222rt_assign1/Exercise_7_13/Play123Main.java
@@ -7,12 +7,13 @@ public class Play123Main {
 
     private static int wonTheGame = 0;
     private static int lostTheGame = 0;
+    private static int numberOfGames = 10000;
     private static double probability = 0.000000000000;
     private static Deck deck;
 
     public static void main(String[] args){
         System.out.println("Starting game...");
-        for (int i = 0; i < 10000; i++){
+        for (int i = 0; i < numberOfGames; i++){
             if (play123()){
                 wonTheGame++;
             }else{
@@ -23,20 +24,22 @@ public class Play123Main {
         if (lostTheGame == 0){
             probability = 100;
         }else{
-            probability = (wonTheGame * 1.0 / lostTheGame) * 100;
+            probability = (wonTheGame * 1.0 / numberOfGames) * 100;
         }
 
         System.out.println("played the game 10000 times.");
         System.out.println("Won games - " + wonTheGame);
         System.out.println("Lost games - " + lostTheGame);
-        System.out.println("The probability to win 1-2-3 is = " + String.format("%.5f", probability) + " %");
+        System.out.println("The probability to win 1-2-3 is = " + String.format("%.2f", probability) + " %");
     }
 
     private static boolean play123(){
         deck = new Deck();
         int count = 0;
-        for (Card c : deck.deck){
+
+        while(deck.deckSize() != 0){
             count++;
+            Card c = deck.handOutNextCards();
             if (count == c.getRank().value){
                 return false;
             }
@@ -45,6 +48,7 @@ public class Play123Main {
                 count = 0;
             }
         }
+
         return true;
     }
 }
